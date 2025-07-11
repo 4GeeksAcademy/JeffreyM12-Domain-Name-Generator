3aa031f6bdf6be660f2aa5ff8beaf696bd57d2b4
JeffreyM12 <62064110+JeffreyM12@users.noreply.github.com>
2025-07-11 13:44:25 +0000

    domain-name

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+ let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extension = ['.com', '.net', '.us', '.io'];
+
 window.onload = function() {
   //write your code here
+
+  for(let a = 0; a < pronoun.length; a++){
+    for(let b = 0; b < adj.length; b++){
+      for(let c = 0; c < noun.length; c++){
+        for(let d = 0; d < extension.length; d++){
+
+          let domain = document.createElement('div');
+
+          //string concatenation
+          domain.innerText = `${pronoun[a]}${adj[b]}${noun[c]}${extension[d]}`;
+
+          //creates a div in the body of HTML
+          document.body.appendChild(domain);
+        }
+      }
+    }
+  }
+
   console.log("Hello Rigo from the console!");
 };
